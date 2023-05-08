c9bca3c612715c09b2cc7f3fb559b64a3d4fb85f
Anton Soares <anton.soares@encora.com>
2023-05-08 17:40:25 -0500

    migrated publish-module.gradle to kts and moved it inside of android-base-lib.gradle.kts.

--- buildSrc/src/main/kotlin/android-base-lib.gradle.kts
+++ buildSrc/src/main/kotlin/android-base-lib.gradle.kts
@@ -1,10 +1,10 @@
-import io.gitlab.arturbosch.detekt.Detekt
-
 plugins {
     id("com.android.library")
     id("io.gitlab.arturbosch.detekt")
     kotlin("android")
     id("jacoco")
+    id("maven-publish")
+    id("signing")
 }
 
 detekt {
@@ -47,12 +47,78 @@ dependencies {
     detektPlugins(BuildPlugins.detekt)
 }
 
-ext {
-    this["PUBLISH_GROUP_ID"] = getPropertyOrFail("group")
-    this["PUBLISH_VERSION"] = getPropertyOrFail("version")
-    this["PUBLISH_ARTIFACT_ID"] = properties.get("artifactId").toString()
+
+// Publishing block
+
+val androidSourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    if (project.plugins.findPlugin("com.android.library") != null) {
+        from("android.sourceSets.main.java.srcDirs")
+        from("android.sourceSets.main.kotlin.srcDirs")
+    } else {
+        from("sourceSets.main.java.srcDirs")
+        from("sourceSets.main.kotlin.srcDirs")
+    }
 }
 
-apply {
-    from("${rootProject.projectDir}/tools/scripts/publish-module.gradle")
+artifacts {
+    add("archives", androidSourcesJar)
+}
+
+val groupProperty = getPropertyOrFail("group")
+val versionProperty = getPropertyOrFail("version")
+val artifactId = properties.get("artifactId").toString()
+
+group = groupProperty
+version = versionProperty
+
+afterEvaluate {
+    publishing {
+        publications {
+            register("release", MavenPublication::class.java) {
+                setupPublication()
+            }
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        rootProject.ext["signing.keyId"].toString(),
+        rootProject.ext["signing.key"].toString(),
+        rootProject.ext["signing.password"].toString(),
+    )
+}
+
+fun MavenPublication.setupPublication() {
+    groupId = groupProperty
+    artifactId = artifactId
+    version = versionProperty
+
+    if (project.project.plugins.findPlugin("com.android.library") != null) {
+        from(project.components["release"])
+    } else {
+        from(project.components["java"])
+    }
+
+    artifact(androidSourcesJar)
+
+    pom {
+        name.set(artifactId)
+        url.set("https://github.com/openmobilehub/omh-auth")
+        licenses {
+            license {
+                name.set("Apache-2.0 License")
+                url.set("https://github.com/openmobilehub/omh-auth/blob/main/LICENSE")
+            }
+        }
+
+        // Version control info - if you're using GitHub, follow the
+        // format as seen here
+        scm {
+            connection.set("scm:git:github.com/openmobilehub/omh-auth.git")
+            developerConnection.set("scm:git:ssh://github.com/openmobilehub/omh-auth.git")
+            url.set("https://github.com/openmobilehub/omh-auth")
+        }
+    }
 }
---
+++ tools/scripts/publish-root.gradle.kts
@@ -0,0 +1,1 @@
+// Create variables with empty default values
