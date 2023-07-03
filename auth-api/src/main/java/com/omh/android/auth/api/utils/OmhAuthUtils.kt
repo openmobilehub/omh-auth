200061dc75403c1a5cbd08ae8ecf7ed44444ab51
Anton Soares <anton.soares@encora.com>
2023-07-03 11:57:22 -0500

    Added an exception to GMS to indicate usage in a non GMS device.

---
+++ auth-api/src/main/java/com/omh/android/auth/api/utils/OmhAuthUtils.kt
@@ -0,0 +1,17 @@
+package com.omh.android.auth.api.utils
+
+import android.content.Context
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+
+object OmhAuthUtils {
+
+    @JvmStatic
+    fun isGmsDevice(context: Context): Boolean {
+        val googleApiAvailability = GoogleApiAvailability.getInstance()
+        return when (googleApiAvailability.isGooglePlayServicesAvailable(context)) {
+            ConnectionResult.SUCCESS -> true
+            else -> false
+        }
+    }
+}
